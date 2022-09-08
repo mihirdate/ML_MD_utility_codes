29f1ac00fa9b85cd1480f21545cf12e1f21b5cc8
mihirdate <mihirdate11@gmail.com>
2022-09-07 21:15:27 -0400

    Update uniprotenator.py

--- uniprotenator.py
+++ uniprotenator.py
@@ -1,3 +1,12 @@
+'''
+uniprotenator
+Small python script to download and view pdb's in pymol all pdb's related to a single uniprot accession number.
+
+USAGE: python uniprotenator.py P68400
+Also 'open -a MacPyMOL *.pdb' and then @P68400_align.pml in pymol.
+Where 'P68400' is the uniprot accession number of your protein.
+'''
+
 # Get all PDB's for a given uniprot accession number.
 
 #    USAGE: python uniprotenator.py P68400
